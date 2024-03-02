569292ec49bcc1f981d7ec0c93f14bcaeb4bbc45
taochanggong <130819421+taochanggong@users.noreply.github.com>
2024-03-03 08:42:17 +1030

    Update test_3_1.c

--- test_3_1.c
+++ test_3_1.c
@@ -8,7 +8,8 @@ int main()
     float math=0.0f;
     float eng=0.0f;
     scanf("%d;%f,%f,%f",&id,&c,&math,&eng);
-    // 输出数据,要小数点之后两位,就写.2
+    // 输出数据,要小数点之后两位,就写.2,如果是整型,比如输入年月日的日期的时候,3月要输出位03,这时要写为%02d, 
+    // 如果不写0就会是输出两位,但是不是用零占位,是输出一个空格.
     printf("The each subject score of No. %d is %.2f, %.2f, %.2f.\n", id, c, math, eng);
     
     return 0;
